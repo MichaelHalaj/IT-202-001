a543d6b4a7c62f66b05d74ced896df7a7b4ec1a2
MichaelHalaj <mh45@njit.edu>
2021-10-13 11:36:22 -0400

    updated Feat-UserProfile

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -7,6 +7,10 @@ require(__DIR__ . "/../../partials/nav.php");
         <input type="email" name="email" required />
     </div>
     <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" required maxlength="30" />
+    </div>
+    <div>
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
@@ -30,6 +34,7 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
     $email = se($_POST, "email", "", false);
     $password = se($_POST, "password", "", false);
     $confirm = se($_POST, "confirm", "", false);
+    $username = se($_POST, "username", "", false);
     //TODO 3
 
 
@@ -43,6 +48,9 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
     $email = sanitize_email($email);
     //validate
     //if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if(!preg_match('/^[a-z0-9_-]{3,30}$/', $username)){
+        flash("Invalid username, must be alpanumeric and only contain letters and numbers");
+    }
     if (!is_valid_email($email)) {
         flash("Invalid email");
         $hasError = true;
