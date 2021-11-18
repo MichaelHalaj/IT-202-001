6ee3817d8d5e696365813f0754ec2fbb70b28c82
MichaelHalaj <mh45@njit.edu>
2021-11-18 18:07:20 -0500

    template copy

--- lib/functions.php
+++ lib/functions.php
@@ -131,3 +131,69 @@ function get_url($dest)
     //handle relative path
     return $BASE_PATH . $dest;
 }
+
+function get_random_str($length)
+{
+    return substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 36)), 0, $length);
+}
+function get_or_create_account()
+{
+    if (is_logged_in()) {
+        //let's define our data structure first
+        //id is for internal references, account_number is user facing info, and balance will be a cached value of activity
+        $account = ["id" => -1, "account_number" => false, "balance" => 0];
+        //this should always be 0 or 1, but being safe
+        $query = "SELECT id, account, balance from Bank_Accounts where user_id = :uid LIMIT 1";
+        $db = getDB();
+        $stmt = $db->prepare($query);
+        try {
+            $stmt->execute([":uid" => get_user_id()]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (!$result) {
+                //account doesn't exist, create it
+                $created = false;
+                //we're going to loop here in the off chance that there's a duplicate
+                //it shouldn't be too likely to occur with a length of 12, but it's still worth handling such a scenario
+
+                //you only need to prepare once
+                $query = "INSERT INTO Bank_Accounts (account, user_id) VALUES (:an, :uid)";
+                $stmt = $db->prepare($query);
+                $user_id = get_user_id(); //caching a reference
+                $account_number = "";
+                while (!$created) {
+                    try {
+                        $account_number = get_random_str(12);
+                        $stmt->execute([":an" => $account_number, ":uid" => $user_id]);
+                        $created = true; //if we got here it was a success, let's exit
+                        flash("Welcome! Your account has been created successfully", "success");
+                    } catch (PDOException $e) {
+                        $code = se($e->errorInfo, 0, "00000", false);
+                        //if it's a duplicate error, just let the loop happen
+                        //otherwise throw the error since it's likely something looping won't resolve
+                        //and we don't want to get stuck here forever
+                        if (
+                            $code !== "23000"
+                        ) {
+                            throw $e;
+                        }
+                    }
+                }
+                //loop exited, let's assign the new values
+                $account["id"] = $db->lastInsertId();
+                $account["account_number"] = $account_number;
+            } else {
+                //$account = $result; //just copy it over
+                $account["id"] = $result["id"];
+                $account["account_number"] = $result["account"];
+                $account["balance"] = $result["balance"];
+            }
+        } catch (PDOException $e) {
+            flash("Technical error: " . var_export($e->errorInfo, true), "danger");
+        }
+        $_SESSION["user"]["account"] = $account; //storing the account info as a key under the user session
+        //Note: if there's an error it'll initialize to the "empty" definition around line 161
+
+    } else {
+        flash("You're not logged in", "danger");
+    }
+}
