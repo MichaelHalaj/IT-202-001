3dd57b473486a38de270c9e1cc08ce40931b6e7f
MichaelHalaj <mh45@njit.edu>
2021-11-29 12:14:59 -0500

    transaction basics working

--- lib/functions.php
+++ lib/functions.php
@@ -145,7 +145,7 @@ function refresh_account_balance()
         $stmt = $db->prepare($query);
         try {
             $stmt->execute([":src" => get_user_account_id()]);
-            //get_or_create_account(); //refresh session data
+            get_account(); //refresh session data
         } catch (PDOException $e) {
             flash("Error refreshing account: " . var_export($e->errorInfo, true), "danger");
         }
@@ -155,7 +155,7 @@ function transaction($money, $typeTrans, $src = -1, $dest = -1, $memo = "")
 {
     //I'm choosing to ignore the record of 0 point transactions
     if ($money > 0) {
-        $query = "INSERT INTO Bank_Account_Transactions (src, dest, diff, reason, memo) 
+        $query = "INSERT INTO Bank_Account_Transactions (src, dest, diff, typeTrans, memo) 
             VALUES (:acs, :acd, :pc, :r,:m), 
             (:acs2, :acd2, :pc2, :r, :m)";
         //I'll insert both records at once, note the placeholders kept the same and the ones changed.
@@ -175,9 +175,9 @@ function transaction($money, $typeTrans, $src = -1, $dest = -1, $memo = "")
             //Only refresh the balance of the user if the logged in user's account is part of the transfer
             //this is needed so future features don't waste time/resources or potentially cause an error when a calculation
             //occurs without a logged in user
-            if ($src == get_user_account_id() || $dest == get_user_account_id()) {
+           // if ($src == get_user_account_id() || $dest == get_user_account_id()) {
                 refresh_account_balance();
-            }
+           // }
         } catch (PDOException $e) {
             flash("Transfer error occurred: " . var_export($e->errorInfo, true), "danger");
         }
@@ -187,14 +187,14 @@ function get_random_str($length)
 {
     return substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 36)), 0, $length);
 }
-function get_or_create_account()
+function get_or_create_account($accountType, $money)
 {
     if (is_logged_in()) {
         //let's define our data structure first
         //id is for internal references, account_number is user facing info, and balance will be a cached value of activity
         $account = ["id" => -1, "account_number" => false, "balance" => 0];
         //this should always be 0 or 1, but being safe
-        $query = "SELECT id, account, balance from Bank_Accounts where user_id = :uid LIMIT 1";
+        $query = "SELECT id, account, balance, account_type from Bank_Accounts where user_id = :uid LIMIT 1";
         $db = getDB();
         $stmt = $db->prepare($query);
         try {
@@ -206,15 +206,17 @@ function get_or_create_account()
                 //it shouldn't be too likely to occur with a length of 12, but it's still worth handling such a scenario
 
                 //you only need to prepare once
-                $query = "INSERT INTO Bank_Accounts (account, user_id) VALUES (:an, :uid)";
+                $query = "INSERT INTO Bank_Accounts (account, user_id, account_type) VALUES (:an, :uid, :at)";
                 $stmt = $db->prepare($query);
                 $user_id = get_user_id(); //caching a reference
                 $account_number = "";
                 while (!$created) {
                     try {
                         $account_number = get_random_str(12);
-                        $stmt->execute([":an" => $account_number, ":uid" => $user_id]);
-                        $created = true; //if we got here it was a success, let's exit
+                        $stmt->execute([":an" => $account_number, ":uid" => $user_id, ":at" => $accountType]);
+                        $created = true;
+                        //transaction($money, "deposit", -1, $account_number, ""); 
+                        //if we got here it was a success, let's exit
                         flash("Welcome! Your account has been created successfully", "success");
                     } catch (PDOException $e) {
                         $code = se($e->errorInfo, 0, "00000", false);
@@ -231,6 +233,35 @@ function get_or_create_account()
                 //loop exited, let's assign the new values
                 $account["id"] = $db->lastInsertId();
                 $account["account_number"] = $account_number;
+        } catch (PDOException $e) {
+            flash("Technical error: " . var_export($e->errorInfo, true), "danger");
+        }
+        $_SESSION["user"]["account"] = $account; //storing the account info as a key under the user session
+        //Note: if there's an error it'll initialize to the "empty" definition around line 161
+
+    } else {
+        flash("You're not logged in", "danger");
+    }
+}
+function get_account()
+{
+    if (is_logged_in()) {
+        //let's define our data structure first
+        //id is for internal references, account_number is user facing info, and balance will be a cached value of activity
+        $account = ["id" => -1, "account_number" => false, "balance" => 0];
+        //this should always be 0 or 1, but being safe
+        $query = "SELECT id, account, balance from Bank_Accounts where user_id = :uid LIMIT 1";
+        $db = getDB();
+        $stmt = $db->prepare($query);
+        try {
+            $stmt->execute([":uid" => get_user_id()]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                //$account = $result; //just copy it over
+                $account["id"] = $result["id"];
+                $account["account_number"] = $result["account"];
+                $account["balance"] = $result["balance"];
+            
         } catch (PDOException $e) {
             flash("Technical error: " . var_export($e->errorInfo, true), "danger");
         }
--- public_html/Project/account.php
+++ public_html/Project/account.php
@@ -5,38 +5,54 @@ require(__DIR__ . "/../../partials/nav.php");
 //(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$
 
 if(isset($_POST["save"])){
-    $radioVal = $_POST["radio"];
-    echo $radioVal;
+  //  $db = getDB();
     $accountType = se($_POST, "radio", null, false);
-    echo $accountType;
-    $depositAmount = se($_POST, "despositAmount", null, false);
-    echo "<pre>" . var_export($accountType, true) . "</pre>";
-    get_or_create_account();
-   // transaction($depositAmount, "deposit", -1, )
+    $depositAmount = se($_POST, "deposit", "", false);
+   // echo var_export($depositAmount);
+    transaction($depositAmount, "deposit", -1, 1, "");
+    get_or_create_account($accountType, $depositAmount);
+  // $y = se($_SESSION["user"], "account", "", false);
+   // echo var_export($_SESSION["user"]);
+    //var_export($_SESSION["user"]["account"]["account_number"]);
+    //transaction($depositAmount, "deposit", -1, ;
+    /*
+    $query = "SELECT id from Bank_Accounts where account = :account LIMIT 1";
+    
+    $stmt = $db->prepare($query);
+    //$stmt->bind_param("s", $_SESSION["user"]["account"]["account_number"]);
+
+        $stmt->execute([":account" => $_SESSION["user"]["account"]["account_number"]]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC); */
+        //$result = $stmt->get_result();
+        //$row = $result->fetch_assoc();
+    //echo var_export($result);
+    // transaction($depositAmount, "deposit", -1, $db->lastInsertID(), "");
+
+    
 }
 ?>
 <div class="container-fluid">
 <?php if (is_logged_in()) : ?>
     <form onsubmit="return validate(this)" method="POST">
         <div class="form-check">
-                <input class="form-check-input" type="radio" name="radio" id="Checking" checked>
+                <input class="form-check-input" type="radio" name="radio" value = "checking" id="checking" checked>
                 <label class="form-check-label" for="Checking">
                     Checking
                 </label>
         </div>
         <div class="form-check">
-                <input class="form-check-input" type="radio" name="radio" id="Savings">
+                <input class="form-check-input" type="radio" name="radio" value = "savings" id="savings">
                 <label class="form-check-label" for="Savings">
                     Savings
                 </label>
         </div>
             <div class="mb-3 form-group col-md-3">
                     <h2 label class="form-label text-dark" for="da">Deposit</h2>
-                    <input class="form-control" type="number" input type="number" min="5.00" step="0.01"  name="depositAmount" id="da" />
+                    <input class="form-control" type="number" input type="number" min="5.00" step="0.01"  name="deposit"  id="da" />
                     <small id="minimumDeposit"  class="form-text text-warning">A minimum deposit of $5.00 must be made.</small>
 
             </div>
-            <input type="submit" class="btn btn-success" value = "Create Account" name = "save"></input>
+            <input type="submit" class="btn btn-success" value = "Create Account" name = "save" ></input>
     </form>
     <?php endif; ?>
 </div>
