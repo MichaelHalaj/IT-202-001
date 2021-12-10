d7a408fce478a3810f2026f1a4f123e0200d2809
MichaelHalaj <mh45@njit.edu>
2021-12-10 18:08:51 -0500

    mostly working

--- public_html/Project/transactions.php
+++ public_html/Project/transactions.php
@@ -1,98 +1,101 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 
-
+$db = getDB();
 $total_query = "SELECT count(1) as total FROM Bank_Account_Transactions t WHERE 1=1";
-$base_query = "SELECT t.id, t.diff, t.typeTrans, t.memo, t.created /*IF(a2.account = '0', 'system', a2.account) as 'account' */ FROM Bank_Account_Transactions t JOIN 
+$base_query = "SELECT t.id, t.src, t.diff, t.typeTrans, t.memo, t.created /*IF(a2.account = '0', 'system', a2.account) as 'account' */ FROM Bank_Account_Transactions t JOIN 
 Bank_Accounts a1 on a1.id = t.src JOIN Bank_Accounts a2 on a2.id = t.dest WHERE 1=1";
 
-
-$account = se($_GET, "account", "" , false);
-$account = find_account($account);
-
-$type = se($_GET, "type", null, false);
-$start = se($_GET, "date1", date("Y-m-d", strtotime("-1 month")));
 $end = se($_GET, "date2", date("Y-m-d"), false);
-$order = se($_GET, "order", "asc", false);
-$params = [];
-if (!in_array($order, ["asc", "desc"])) {
-    $order = "asc"; //default value, prevent sql injection
-}
+$account = se($_GET, "account", "" , false);
+if(strlen($account)===12){
+    $account = find_account($account);
 
-if($start){
-    $total_query .= " AND t.created >= :start";
-    $base_query .= " AND t.created >= :start";
-    $params[":start"] = $start;
-}
-if($end){
-    $total_query .= " AND t.created <= :end";
-    $base_query .= " AND t.created <= :end";
-    $params[":end"] = date("Y-m-d 23:59:59", strtotime($end));
-}
-if($type && $type !== "none"){
-    $total_query .= " AND typeTrans = :typeTrans";
-    $base_query .= " AND typeTrans = :typeTrans";
-    $params[":typeTrans"] = $type;
-}
-$total_query .= " AND src = :src";
-$base_query .= " AND src = :src";
-$params[":src"] = $account;
-
-$sort = se($_GET, "sort", "", false);
-switch($sort){
-    case "+date":
-        $total_query .= " ORDER BY created asc";
-        $base_query .= " ORDER BY created asc";
-        break;
-    case "-date":
-        $total_query .= " ORDER BY created desc";
-        $base_query .= " ORDER BY created desc";
-        break;
-    case "+change":
-        $total_query .= " ORDER BY diff asc";
-        $base_query .= " ORDER BY diff asc";
-        break;
-    case "-change":
-        $total_query .= " ORDER BY diff desc";
-        $base_query .= " ORDER BY diff desc";
-        break;
-}
-$records_per_page = 10;
-$total_records = 0;
-$db = getDB();
-$stmt = $db->prepare($total_query);
-try{
-    $stmt->execute($params);
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    if($result){
-        $total_records = se($result, "total", 0, false);
+    $type = se($_GET, "type", null, false);
+    $start = se($_GET, "date1", date("Y-m-d", strtotime("-1 month")));
+   
+    $order = se($_GET, "order", "asc", false);
+    $params = [];
+    if (!in_array($order, ["asc", "desc"])) {
+        $order = "asc"; //default value, prevent sql injection
     }
-}catch(PDOException $e){
-    flash("<pre>" . var_export($e, true) . "</pre>");
+    
+    if($start){
+        $total_query .= " AND t.created >= :start";
+        $base_query .= " AND t.created >= :start";
+        $params[":start"] = $start;
+    }
+    if($end){
+        $total_query .= " AND t.created <= :end";
+        $base_query .= " AND t.created <= :end";
+        $params[":end"] = date("Y-m-d 23:59:59", strtotime($end));
+    }
+    if($type && $type !== "none"){
+        $total_query .= " AND typeTrans = :typeTrans";
+        $base_query .= " AND typeTrans = :typeTrans";
+        $params[":typeTrans"] = $type;
+    }
+    $total_query .= " AND src = :src";
+    $base_query .= " AND src = :src";
+    $params[":src"] = $account;
+    
+    $sort = se($_GET, "sort", "", false);
+    switch($sort){
+        case "+date":
+            $total_query .= " ORDER BY created asc";
+            $base_query .= " ORDER BY created asc";
+            break;
+        case "-date":
+            $total_query .= " ORDER BY created desc";
+            $base_query .= " ORDER BY created desc";
+            break;
+        case "+change":
+            $total_query .= " ORDER BY diff asc";
+            $base_query .= " ORDER BY diff asc";
+            break;
+        case "-change":
+            $total_query .= " ORDER BY diff desc";
+            $base_query .= " ORDER BY diff desc";
+            break;
+    }
+    $records_per_page = 10;
+    $total_records = 0;
+
+    $stmt = $db->prepare($total_query);
+    try{
+        $stmt->execute($params);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($result){
+            $total_records = se($result, "total", 0, false);
+        }
+    }catch(PDOException $e){
+        flash("<pre>" . var_export($e, true) . "</pre>");
+    }
+    
+    $page = se($_GET, "page", 1, false);
+    if($page < 1){
+        $page = 1;
+    }
+    
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    
+    $offset = ($page -1) * $records_per_page;
+    $base_query .= " LIMIT :offset, :limit";
+    $result = [];
+    
+    $stmt = $db->prepare($base_query);
+    try{
+        $params[":offset"] = $offset;
+        $params[":limit"] = $records_per_page;
+        $stmt->execute($params);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }catch (PDOException $e){
+        flash("<pre>" . var_export($e, true) . "</pre>");
+    }
+    $total_pages = ceil($total_records/$records_per_page);
 }
 
-$page = se($_GET, "page", 1, false);
-if($page < 1){
-    $page = 1;
-}
-
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-$offset = ($page -1) * $records_per_page;
-$base_query .= " LIMIT :offset, :limit";
-$result = [];
-
-$stmt = $db->prepare($base_query);
-try{
-    $params[":offset"] = $offset;
-    $params[":limit"] = $records_per_page;
-    $stmt->execute($params);
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-}catch (PDOException $e){
-    flash("<pre>" . var_export($e, true) . "</pre>");
-}
-$total_pages = ceil($total_records/$records_per_page);
-echo var_export($results);
+//echo var_export($results);
 $options = [];
 $query = "SELECT DISTINCT typeTrans from Bank_Account_Transactions";
 $stmt = $db->prepare($query);
@@ -101,7 +104,7 @@ try{
     $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
     if($r){
         $options = $r;
-        echo var_export($options);
+        //echo var_export($options);
     }
 }catch(PDOException $e){
     flash("<pre>" . var_export($e, true) . "</pre>");
@@ -206,8 +209,7 @@ try {
     <?php endif?>
     <table class=" table text-light">
                             <thead>
-                                <th>Account</th>
-                            
+                                <th>Account ID</th>
                                 <th>Amount</th>
                                 <th>Type</th>
                                 <th>Memo</th>
@@ -221,7 +223,7 @@ try {
                                 <?php else : ?>
                             <?php foreach ($results as $h) : ?>
                                 <tr>
-                                    <td><?php find_account(se($h, "src"))?></td>
+                                    <td><?php se($h,"src") ?></td>
                                     <td class = "bal"><?php se($h, "diff"); ?></td>
                                     <td><?php se($h, "typeTrans"); ?></td>
                                     <td><?php se($h, "memo");?></td>
@@ -240,7 +242,7 @@ try {
             $total_pages =1;   
         }?>
         <ul class = "pagination">
-            <li class = "page-item <?php if (($pafe -1) < 1) echo 'disabled';?>">
+            <li class = "page-item <?php if (($page -1) < 1) echo 'disabled';?>">
                 <a class = "page-link" href = "?<?php pagination_filter($page -1); ?>">Previous</a>
             </li>
             <?php for($i = 1; $i <= $total_pages; $i++) ?>
@@ -253,6 +255,19 @@ try {
             </li>    
         </ul>
 </div>
+<script>
+        var x = document.getElementsByClassName("bal");
+        for(let i of x){
+        let y = i.innerHTML;
+        if(y < 0){
+            y*=-1;
+        }
+        i.innerHTML = parseInt(y)/100;
+        console.log(i.innerHTML);
+    
+    }
+
+</script>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
